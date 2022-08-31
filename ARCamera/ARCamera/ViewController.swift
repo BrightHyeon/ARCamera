1174e3b1beebf25fc3550d6440b8b4eb93a8ef23
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-08-31 16:52:31 +0900

    initial commit

---
+++ ARCamera/ARCamera/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ARCamera
+//
+//  Created by Hyeonsoo Kim on 2022/08/31.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
